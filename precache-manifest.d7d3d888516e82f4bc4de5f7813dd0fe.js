1ee491ead584c8866a85ee51eb044bb0204bb03d
Diego Chavarria <diego.chavarriap@gmail.com>
2020-06-13 23:16:23 -0500

    Updates

---
+++ precache-manifest.d7d3d888516e82f4bc4de5f7813dd0fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802bc0b3d8b99c0da1ac447e5b83b6ce",
+    "url": "/Transactions-frontend/index.html"
+  },
+  {
+    "revision": "8ec324c255083630064a",
+    "url": "/Transactions-frontend/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "98207b99a744cc756fb7",
+    "url": "/Transactions-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ec324c255083630064a",
+    "url": "/Transactions-frontend/static/js/2.73ba64e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Transactions-frontend/static/js/2.73ba64e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98207b99a744cc756fb7",
+    "url": "/Transactions-frontend/static/js/main.9fb64b8a.chunk.js"
+  },
+  {
+    "revision": "b688bd6cdcf7c6b29427",
+    "url": "/Transactions-frontend/static/js/runtime-main.5844dc64.js"
+  }
+]);
